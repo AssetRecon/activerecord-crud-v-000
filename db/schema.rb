a80fc6d5287ad85eff487b5751b156617bfae1e4
Edsel Caprice <caprice@mail.usf.edu>
2018-08-22 02:31:44 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2018_08_22_020555) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.integer "release_date"
+    t.string "director"
     t.string "lead"
     t.boolean "in_theaters"
   end
